bef442d80a738f77b471e8a348d98e2c3fad8a58
lybenson <naivebenson@gmail.com>
2018-06-08 13:54:42 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      Eth
+    </div>
+  </section>
+</template>
+
+<script>
+import web3 from '../libs/web3'
+import ProjectList from '../libs/ProjectList'
+import Project from '../libs/Project'
+
+export default {
+  async asyncData () {
+    const addressList = await proj
+  },
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
