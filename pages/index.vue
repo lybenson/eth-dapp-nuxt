915f29a0d891c99292d578439403e03582a251e1
lybenson <naivebenson@gmail.com>
2018-06-10 00:46:01 +0800

    invest

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,12 @@
       余额: {{weiToEth(x.balance)}} eth
     </div>
 
+    <nuxt-link :to="'/project/create'"><el-button>创建项目</el-button></nuxt-link>
+
     <h1>项目列表</h1>
-    <!-- <div v-for="(project, index) in projects" :key="index">
-      {{project}}
-    </div> -->
     <el-table :data="projects" border>
       <el-table-column label="项目地址" prop="address" align="center"></el-table-column>
-      <el-table-column label="项目描述" prop="description" align="center"></el-table-column>
+      <el-table-column label="项目名称" prop="description" align="center"></el-table-column>
       <el-table-column label="募资上限" prop="goal" align="center"></el-table-column>
       <el-table-column label="最小投资金额" prop="minInvest" align="center"></el-table-column>
       <el-table-column label="最大投资金额" prop="maxInvest" align="center"></el-table-column>
@@ -21,7 +20,7 @@
 
       <el-table-column label="操作" align="center">
         <template slot-scope="scope">
-          <el-button type="primary">立即投资</el-button>
+          <el-button type="primary" @click="invest(scope.row)">立即投资</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -68,12 +67,6 @@ export default {
         owner,
       }
     }).reverse()
-    // try {
-    //   const projects = await ProjectList.methods.getProjects().call()
-    //   console.log(projects)
-    // } catch (error) {
-    //   console.log(error)
-    // }
 
     return {
       accounts: accounts.map((x, i) => ({ account: x, balance: balances[i] })),
@@ -90,6 +83,48 @@ export default {
   methods: {
     weiToEth (wei) {
       return web3.utils.fromWei(wei, 'ether')
+    },
+    async invest (project) {
+      console.log(project)
+      let { value } = await this.$prompt('请输入投资金额', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        closeOnClickModal: false,
+        closeOnPressEscape: false
+      })
+
+      if (value <= 0) {
+        this.$message.error('投资金额必须大于0')
+        return
+      }
+      if (value < project.minInvest) {
+        this.$message.error('投资金额必须大于最小投资金额')
+        return
+      }
+      if (value > project.maxInvest) {
+        this.$message.error('投资金额必须小于最大投资金额')
+        return
+      }
+
+      try {
+        // 获取账户
+        const accounts = await web3.eth.getAccounts()
+        const owner = accounts[0]
+
+        // 发起转账
+        const contract = Project(project.address)
+        const result = await contract.methods
+          .contribute()
+          .send({ from: owner, value: web3.utils.toWei(value, 'ether'), gas: '5000000' })
+
+        this.$message.error('投资成功')
+        setTimeout(() => {
+          location.reload();
+        }, 1000)
+      } catch (error) {
+        console.log(error)
+        this.$message.error('投资失败')
+      }
     }
   } 
 }
